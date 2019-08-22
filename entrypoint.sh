6d7a0856aaa887f20c2c45a4a724e64fda0eca57
Bart Dabek <bart@flowpress.com>
2019-08-22 15:01:47 -0400

    Update action

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,14 +14,17 @@ pwd
 # sass-lint --config .scss-lint.yml wp-content/themes/**/*.scss  --verbose --no-exit
 
 OUTPUT="$(sass-lint --config .scss-lint.yml **/*.scss  --verbose --no-exit)"
+
+echo 'output'
 echo "${OUTPUT}"
 
+echo 'multiline'
 MULTILINE=$(ls \
    -1)
 echo "${MULTILINE}"
 
 
-if [[ $string == *"warning"* ]]; then
+if [[ $OUTPUT == *"warning"* ]]; then
 	echo "SCSS Linting Failed";
 	exit 1
 fi
