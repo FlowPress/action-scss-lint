b0852916acbd39fae08f775783a0aafab452c0ea
Bart Dabek <bart@flowpress.com>
2019-08-26 13:22:40 -0400

    Clean up script

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,28 +3,17 @@
 npm install -g sass-lint 
 
 
-pwd
 cd wp-content/themes/
-pwd
 cd "$(dirname "$(find . -type f -name "".scss-lint.yml | head -1)")"
-pwd
-# sass-lint --config .scss-lint.yml **/*.scss  --verbose --no-exit
-
-
-# sass-lint --config .scss-lint.yml wp-content/themes/**/*.scss  --verbose --no-exit
 
 OUTPUT="$(sass-lint --config .scss-lint.yml **/*.scss  --verbose --no-exit)"
 
-echo 'output'
 echo "${OUTPUT}"
 
 single=(tr -d '\n' < $OUTPUT)
 
-echo $single
-
-
 if [[ echo "$single" == *"warning"* ]]; then
-	echo "SCSS Linting Failed.";
+	echo "SCSS Linting Failed."
 	exit 1
 else 
 	echo "SCSS Passed."
